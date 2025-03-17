b767fbe8fba441b68a56af23dea744569186be24
VypeX <140086046+VypeXis@users.noreply.github.com>
2025-03-17 01:06:54 -0300

    Add files via upload

---
+++ bla.ps1
@@ -0,0 +1,7 @@
+
+${nO`Me} = ("{1}{0}" -f'ndo','Mu')
+${dAta`At`U`AL} = &("{1}{0}" -f't-Date','Ge')
+&("{0}{2}{1}"-f 'Wr','te-Host','i') ('O'+'l�, '+"$nome! "+'A '+'dat'+'a '+'d'+'e '+'hoje'+' '+'�:'+' '+"$dataAtual")
+
+
+&("{0}{2}{1}{3}" -f 'R','ad-','e','Host') -Prompt ("{0}{8}{7}{6}{3}{2}{5}{4}{9}{1}" -f 'P','uar...',' E','e','ra co','nter pa','n','io','ress','ntin')
